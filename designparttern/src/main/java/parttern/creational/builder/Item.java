57849e35be8ac74502c29c1743cf89111011f52b
wanghaipeng <whp15369657805@163.com>
2018-05-17 18:44:20 +0800

    zk+curator实现分布式事务以及选举问题

---
+++ designparttern/src/main/java/parttern/creational/builder/Item.java
@@ -0,0 +1,11 @@
+package parttern.creational.builder;
+
+/**
+ * @author whp 18-4-20
+ */
+public interface Item {
+    public String name();
+    public float price();
+    public Packing packing();
+}
+
