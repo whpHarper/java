fb68c60be9c04627eb954f8c105a34f6a6905bc4
wanghaipeng <whp15369657805@163.com>
2018-07-04 15:25:07 +0800

    修改多余代码

--- storm/src/main/java/Topology.java
+++ storm/src/main/java/Topology.java
@@ -9,11 +9,9 @@ public class Topology {
     public static void main(String[] args) throws InterruptedException {
         TopologyBuilder builder = new TopologyBuilder();
         builder.setSpout("word-reader", new WordReaderSpout());
-        builder.setBolt("word-normalizer", new WordNormalizer()).shuffleGrouping("word-reader");
-        builder.setBolt("word-counter", new SaveToMysqlBolt()).shuffleGrouping("word-normalizer");
+        builder.setBolt("word-counter", new SaveToMysqlBolt()).shuffleGrouping("word-reader");
         Config conf = new Config();
         conf.put(Config.TOPOLOGY_MAX_TASK_PARALLELISM, 1);
-        //conf.put(Config.STORM_LOCAL_DIR,"/Users/merlin/Documents/eclipse-workspace/ideaProject/hive-resource/src/test/java/com/example/demo/storm");
         conf.put("wordsFile", "/mnt/domain.log");
         conf.setDebug(false);
 
