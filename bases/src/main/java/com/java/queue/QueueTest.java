ae6dca1817f4f32b9f3157f5f09ae69db114dd14
fangzhimeng <fangzhimeng0808@126.com>
2019-07-30 21:14:39 +0800

    代码整理

--- bases/src/main/java/com/java/queue/QueueTest.java
+++ bases/src/main/java/com/java/queue/QueueTest.java
@@ -2,16 +2,65 @@ package com.java.queue;
 
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * @author whp 19-1-14
  */
 public class QueueTest {
     public static void main(String[] args){
-        BlockingQueue<String> arrayQueue=new ArrayBlockingQueue<String>(3);
 
+        BlockingQueue<String> arrayQueue=new ArrayBlockingQueue<String>(3);
+        ExecutorService pool = Executors.newFixedThreadPool(6);
+        PutThread putThread=new PutThread(arrayQueue);
+        TakeThread takeThread=new TakeThread(arrayQueue);
+        pool.execute(putThread);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        pool.execute(takeThread);
+//        pool.shutdown();
     }
 
 }
 
-class takeTest extends Thread(){}
+class TakeThread extends Thread{
+    private BlockingQueue<String> blockingQueue;
+    public TakeThread(BlockingQueue<String> blockingQueue){
+        this.blockingQueue=blockingQueue;
+    }
+    public void run(){
+        try {
+            System.out.println("block获取：");
+            while(!blockingQueue.isEmpty()) {
+                String a=blockingQueue.take();
+                System.out.println("取出"+a);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class PutThread extends Thread{
+    private BlockingQueue<String> blockingQueue;
+    public PutThread(BlockingQueue<String> blockingQueue){
+        this.blockingQueue=blockingQueue;
+    }
+
+    public void run(){
+        System.out.println("block插入：");
+        for (int i=0;i<10;i++){
+            try {
+                Thread.sleep(1000);
+                blockingQueue.put("第"+i+"个值");
+                System.out.println("放入第"+i+"个值");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,12 @@
+本工程为java代码积累学习工程，各模块介绍如下
+1. bases涉及java基础内容，包含java8新特性，反射示例代码，jmx，内存获取方法，队列，反射
+2. concurrent 涉及并发知识：
+    com.task.pool 完整线程池代码示例
+    simple.article 为《java并发编程》示例
+3. curator 为zk实现分布式事务管理代码示例
+4. designparttern 为设计模式回顾
+5. hadoop为连接hadoop大数据组件代码示例
+6. kafka 官网kafka stream示例代码
+7. miaosha 秒杀系统设计
+8. storm  storm代码示例（1.1.0版本）
+9. utils 收录常用util类，目前收录linux执行命令代码
